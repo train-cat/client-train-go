c42f77cffd75c845b97a38c0c3e46b14ab704cb8
Eraac <kevin@labesse.me>
2017-11-21 00:43:33 +0100

    post action

--- action.go
+++ action.go
@@ -1,14 +1,46 @@
 package traincat
 
+import (
+	"errors"
+	"net/http"
+)
+
 // Type of action available
 const (
+	ActionTypeTelegram  = "telegram"
 	ActionTypeMessenger = "messenger"
 )
 
-// Action output from the API
-type Action struct {
-	Entity
-	Type string            `json:"type"`
-	Data map[string]string `json:"data"`
-	Hateoas
+type (
+	// Action output from the API
+	Action struct {
+		Entity
+		Type string            `json:"type"`
+		Data map[string]string `json:"data"`
+		Hateoas
+	}
+
+	// ActionInput for the API
+	ActionInput struct {
+		Type string            `json:"type"`
+		Data map[string]string `json:"data"`
+	}
+)
+
+// PostAction add nex action to the API
+func PostAction(i ActionInput) (*Action, error) {
+	refreshAuth()
+
+	a := &Action{}
+
+	resp, err := r(true).
+		SetBody(i).
+		SetResult(a).
+		Post(EndpointActions)
+
+	if resp.StatusCode() != http.StatusCreated {
+		return nil, errors.New(string(resp.Body()))
+	}
+
+	return a, err
 }
