77dc0dc45c5acf41c0d48d9b6991b5e745fcfacd
Eraac <kevin@labesse.me>
2017-11-24 00:21:07 +0100

    change uint to int

--- filters/stop.go
+++ filters/stop.go
@@ -4,7 +4,7 @@ package filters
 type Stop struct {
 	Pagination
 	TerminusID            *int    `query:"terminus_id"`
-	TrainThroughStationID *uint   `query:"train_through_station_id"`
+	TrainThroughStationID *int    `query:"train_through_station_id"`
 	ScheduledBefore       *string `query:"scheduled_before"`
 	ScheduledAfter        *string `query:"scheduled_after"`
 	ScheduledAt           *string `query:"scheduled_at"`
